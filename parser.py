1b5278395a25dfee714b0a9684abddeb778ddd7d
dogura <daiki.ogura@soliton.co.jp>
2014-04-14 17:09:34 +0900

    init
    success options

---
+++ parser.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from __future__ import (print_function, division, absolute_import, unicode_literals, )
+
+from pyparsing import (
+    Optional,
+    Word,
+    Literal,
+    Keyword,
+    OneOrMore,
+    ZeroOrMore,
+    Group,
+    LineEnd,
+    WordEnd,
+    Suppress,
+    Forward,
+    ParserElement,
+    CharsNotIn,
+    quotedString,
+    removeQuotes,
+)
+from pyparsing import (alphas, alphanums, cppStyleComment, )
+from pyparsing import (ParseException, )
+
+
+BASE_STRINGS = alphanums + "-"
+BASE_WORDS = Word(BASE_STRINGS)
+QUOTED_WORDS = quotedString.addParseAction(removeQuotes)
+END_OF_WORDS = WordEnd(BASE_STRINGS)
+
+
+LineSeparator = Suppress(Literal(';')).setResultsName('separator_token')
+Comments = Optional(cppStyleComment.setResultsName('comment'))
+
+NameDefinitions = BASE_WORDS.setResultsName('name')
+ValDefinitions = OneOrMore(QUOTED_WORDS ^ BASE_WORDS).setResultsName('value')
+VarDefinitions = Group(
+    NameDefinitions + END_OF_WORDS.copy() + ValDefinitions
+)
+
+
+# NestedVar = nestedExpr(opener='{', closer='}', content=VarDefinitions)
+opener, closer = Literal('{'), Literal('}')
+NestedVar = Forward()
+_NestedContent = (
+    VarDefinitions +
+    CharsNotIn('{' + '}' + ParserElement.DEFAULT_WHITE_CHARS).setParseAction(lambda t: t[0].strip())
+)
+NestedVar << (
+    Suppress(opener) +
+    OneOrMore(NestedVar | _NestedContent) +
+    Suppress(closer)
+)
+OptionsDefinitions = Group(
+    Keyword('options').setResultsName('name') + NestedVar.setResultsName('values')
+)
+
+
+Expressions = OneOrMore(
+    VarDefinitions | OptionsDefinitions
+)
+
+
+
+if __name__ == '__main__':
+    # texts = io.open('', 'rt').read().replace('\n', '')
+    # print(texts)
+    p = OneOrMore(
+        Comments +
+        (VarDefinitions | OptionsDefinitions) +
+        LineSeparator
+    )
+    c = open('./testbase.conf').read()
+    # parseFile('./testbase.conf')
+    result = p.parseString(c)
+    print(result)
+    # print(result[3]['name'])
+    # print(result[3]['values'])
+    # print(result[3]['values'][0]['name'])
+    # print(result[3]['values'][0]['value'])
+    # print(result[3]['values'][1])
+    # print(result[3]['values'][1])
+
+
+# def hollerith():
+#     '''Returns a parser for a FORTRAN Hollerith character constant.
+#     '''
+#     intExpr = pp.Word(pp.nums).setParseAction(lambda t: int(t[0]))
+#     stringExpr = pp.Forward()
+#     def countedParseAction(toks):
+#         '''Closure to define the content of stringExpr.
+#         '''
+#         n = toks[0]
+#         contents = pp.CharsNotIn('', exact=n)
+#         stringExpr << (pp.Suppress(pp.CaselessLiteral('H')) + contents)
+#         return None
+#
+#     intExpr.addParseAction(countedParseAction)
+#     return (pp.Suppress(intExpr) + stringExpr)
