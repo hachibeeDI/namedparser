8c769dda24ef0084f5273d18433bf4fddb5a495c
dogura <daiki.ogura@soliton.co.jp>
2014-04-22 17:50:47 +0900

    add test to detect option node is valid

--- namedparser/testsuite/test_suite_for_myserver.py
+++ namedparser/testsuite/test_suite_for_myserver.py
@@ -10,6 +10,23 @@ from namedparser import structures
 
 
 class TestOptionsNode(unittest.TestCase):
+    REQUIRE_NODE_TYPES = (
+        'directory',
+        'check-names',
+        'allow-recursion',
+        'allow-query',
+        'allow-query-cache',
+        'allow-transfer',
+        'max-cache-ttl',
+        'min-retry-time',
+        'max-acache-size',
+        'max-cache-size',
+        'max-journal-size',
+        'version',
+        'dnssec-enable',
+        'dnssec-validation',
+        'empty-zones-enable',
+    )
 
     def setUp(self):
         with open(path.dirname(__file__) + '/resources/named.conf') as f:
@@ -21,33 +38,36 @@ class TestOptionsNode(unittest.TestCase):
         self.assertEqual(self.option_node.node_type, 'options')
 
     def test_has_minimum_requirement(self):
-        require_node_types = (
-            'directory',
-            'check-names',
-            'allow-recursion',
-            'allow-query',
-            'allow-query-cache',
-            'allow-transfer',
-            'max-cache-ttl',
-            'min-retry-time',
-            'max-acache-size',
-            'max-journal-size',
-            'version',
-            'dnssec-enable',
-            'dnssec-validation',
-            'empty-zones-enable',
-        )
         values_in_option = self.option_node.value
-        values_not_contained = [req for req in require_node_types if req not in values_in_option]
+        values_not_contained = [req for req in TestOptionsNode.REQUIRE_NODE_TYPES if req not in values_in_option]
         self.assertEqual(values_not_contained, [])
 
         invalid_node = parser.Parser.parseString('''
         options {
             directory "/var/na/named";
             check-names slave ignore;
+            hoge fooo;
         };
         ''')
         invalid_options_value = invalid_node[0].value
-        values_not_contained = [req for req in require_node_types if req not in invalid_options_value]
+        values_not_contained = [req for req in TestOptionsNode.REQUIRE_NODE_TYPES if req not in invalid_options_value]
         self.assertNotEqual(values_not_contained, [])
 
+    def test_hasnot_uninvited_values(self):
+        values_in_option = self.option_node.value
+        values_not_contained = [var for var in values_in_option if var.node_type not in TestOptionsNode.REQUIRE_NODE_TYPES]
+        self.assertEqual(values_not_contained, [])
+
+        invalid_node = parser.Parser.parseString('''
+        options {
+            directory "/var/na/named";
+            check-names slave ignore;
+            hoge fooo;
+        };
+        ''')
+        invalid_options_value = invalid_node[0].value
+        values_not_contained = [var
+                                for var in invalid_options_value
+                                if var.node_type not in TestOptionsNode.REQUIRE_NODE_TYPES
+                                ]
+        self.assertNotEqual(values_not_contained, [])
